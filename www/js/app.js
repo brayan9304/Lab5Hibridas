54333f88df96b8435d1bf118ce10e1d6296dbc56
jaime <jaime.jimenezisi@gmail.com>
2016-11-07 14:09:38 -0500

    geolocalización agregada y vista modificada

--- www/js/app.js
+++ www/js/app.js
@@ -3,40 +3,70 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var climaCtrl = angular.module('starter', ['ionic']);
 
-  .controller('WeatherCtrl', function ($scope, $http) {
+document.addEventListener("deviceready", onDeviceReady, false);
 
-    var Q = "q=";
+
+function onDeviceReady() {
+  navigator.geolocation.getCurrentPosition(exito, error);
+}
+
+
+climaCtrl.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+climaCtrl.controller('currentLocationWeather', function ($scope, $http) {
+  var options = {timeout: 10000};
+  navigator.geolocation.watchPosition(exito, error, options);
+
+  function exito(position) {
+    var latitud = "lat=" + position.coords.latitude;
+    var longitud = "&lon=" + position.coords.longitude;
     var appid = "&appid=";
-    $scope.city  = "medellin";
     var key = "2aba3adc8f9a3eed10e9d43a47edd216";
     var url = 'http://api.openweathermap.org/data/2.5/weather?';
-    var request = url + Q + $scope.city  + appid + key;
-
-    $scope.changeCity = function() {
-      $scope.city = $scope.buscar;
-    }
+    var request = url + latitud + longitud + appid + key;
 
     $http.get(request).then(function (response) {
-      $scope.weath = response.data;
+      $scope.weather = response.data;
     });
-  })
+  }
 
-  .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  function error(error) {
+    window.alert('code: ' + error.code + '\n' +
+      'message: ' + error.message + '\n');
+  }
 
-        // Don't remove this line unless you know what you are doing. It stops the viewport
-        // from snapping when text inputs are focused. Ionic handles this internally for
-        // a much nicer keyboard experience.
-        cordova.plugins.Keyboard.disableScroll(true);
+  $scope.changeCity = function () {
+    $scope.buscar = '';
+    $scope.city = $scope.buscar;
+    window.alert($scope.city);
+    var Q = "q=";
+    var appid = "&appid=";
+    $scope.city = "bogota";
+    var key = "2aba3adc8f9a3eed10e9d43a47edd216";
+    var url = 'http://api.openweathermap.org/data/2.5/weather?';
+    var request = url + Q + $scope.city + appid + key;
+    $http.get(request).then(function (response) {
+        $scope.weather = response.data;
       }
-      if (window.StatusBar) {
-        StatusBar.styleDefault();
-      }
-    });
-  })
+    )
+  }
+
+});
